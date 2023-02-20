7905a9e3e23d46eef3401ca297d7ab2bbe07930a
nezih <m.nezihkaya@gmail.com>
2023-02-20 17:09:40 -0500

    CukesRunner added

---
+++ src/test/java/com/Runners/CukesRunner.java
@@ -0,0 +1,24 @@
+package com.Runners;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources",
+        glue = "src/test/java/com/stepDefinitions",
+        dryRun = true,
+        tags = "",
+        publish = false
+)
+
+
+
+
+
+
+
+public class CukesRunner {
+}
