41eb778c88f47a579751f4825b0aee6d391f5e7c
nezih <m.nezihkaya@gmail.com>
2023-02-20 14:48:16 -0500

    Configuration Reader added

---
+++ src/test/java/com/Utilities/ConfigurationReader.java
@@ -0,0 +1,28 @@
+package com.Utilities;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConfigurationReader {
+
+    private static Properties properties=new Properties();
+
+    static {
+        FileInputStream File = null;
+        try {
+            File = new FileInputStream("configuration.properties");
+            properties.load(File);
+            File.close();
+        } catch (IOException e) {
+            System.out.println("file is not found");
+            e.printStackTrace();
+        }
+
+    }
+    public static String getProperty(String keyword){
+        return properties.getProperty(keyword);
+    }
+}
