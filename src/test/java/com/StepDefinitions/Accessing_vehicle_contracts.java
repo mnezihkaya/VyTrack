d2511242cbff3498b8774c4641b3d88f1aa9411e
nezih <m.nezihkaya@gmail.com>
2023-03-12 16:53:14 -0400

    Accessing_vehicle_contracts step definition tested

--- src/test/java/com/StepDefinitions/Accessing_vehicle_contracts.java
+++ src/test/java/com/StepDefinitions/Accessing_vehicle_contracts.java
@@ -1,22 +1,35 @@
 package com.StepDefinitions;
 
+import com.Utilities.Driver;
+import com.Utilities.Utilities;
+import com.Utilities.VytrackUtilities;
+import com.pages.MainModule;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
+import org.junit.Assert;
 
 public class Accessing_vehicle_contracts {
+
+    MainModule mainModule;
     @And("user navigates to {string} module and clicks {string}")
-    public void userNavigatesToModuleAndClicks(String arg0, String arg1) {
+    public void userNavigatesToModuleAndClicks(String module, String submodule) {
+        VytrackUtilities.navigateTo(submodule);
     }
 
     @Then("user should see expected URL {string}")
-    public void userShouldSeeExpectedURL(String arg0) {
+    public void userShouldSeeExpectedURL(String expectedURL) {
+        Assert.assertEquals(Driver.getDriver().getCurrentUrl(),expectedURL);
     }
 
     @Then("users should see expected title {string}")
-    public void usersShouldSeeExpectedTitle(String arg0) {
+    public void usersShouldSeeExpectedTitle(String expectedTitle) {
+        Assert.assertEquals(Driver.getDriver().getTitle(),expectedTitle);
     }
 
     @Then("user should see error message {string}")
-    public void userShouldSeeErrorMessage(String arg0) {
+    public void userShouldSeeErrorMessage(String expectedErrorMessage) {
+        Utilities.highlight(mainModule.warningMessage);
+        Assert.assertTrue(mainModule.warningMessage.isDisplayed());
+        Assert.assertEquals(mainModule.warningMessage.getText(),expectedErrorMessage);
     }
 }
