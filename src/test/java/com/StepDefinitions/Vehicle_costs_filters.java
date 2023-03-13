de63a11c14a1e33eb7502d7aef6b95979d21fc7d
nezih <m.nezihkaya@gmail.com>
2023-03-12 20:16:19 -0400

    Vehicle_costsFilters added

---
+++ src/test/java/com/StepDefinitions/Vehicle_costs_filters.java
@@ -0,0 +1,23 @@
+package com.StepDefinitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+
+public class Vehicle_costs_filters {
+    @Then("user should see {int} three columns")
+    public void userShouldSeeThreeColumns(int arg0) {
+    }
+
+    @Then("user should see names of columns")
+    public void userShouldSeeNamesOfColumns() {
+    }
+
+    @Then("user should be able to use all filters")
+    public void userShouldBeAbleToUseAllFilters() {
+    }
+
+    @And("user click firs checkbox")
+    public void userClickFirsCheckbox() {
+    }
+
+}
